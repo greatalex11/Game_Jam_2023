82005fd952ca395ed06ece32edf00fa8718d2a3d
Wika <127410155+FloryneH@users.noreply.github.com>
2023-07-06 10:41:42 +0200

    feat/floryne/01 : js map corridor

--- sketch.js
+++ sketch.js
@@ -2,29 +2,48 @@ let player, floor;
 
 function setup() {
    new Canvas(800, 600);
-   world.gravity.x = 10;
+
+   world.gravity.x = 0;
    world.gravity.y = 10;
 
-   player = new Sprite((0, 255, 0), 0);
-   player.img = "./assets/img/pngwing.com.png";
-   player.scale = 0.01;
-   floor = new Sprite(250, 200, 500, 20, "static");
-   obstacle = new Sprite(550, 180, 100, 100, "static");
+   corridor = new Sprite (0, 300, 19019, 600, 's');
+   corridor.img = "./assets/img/gros couloir.png";
+   corridor.scale = 0.56;
+
+   kitchen = new Sprite (18018, 300, 18538, 600, 's');
+   kitchen.img = "./assets/img/cuisine.png";
+   kitchen.scale = 0.56;
+
+   player = new Sprite(-4700, 0, 19, 36, 'd');
+   player.img = "./assets/img/canne1.png";
+   player.scale = 5;
+   player.rotation = 0;
+
+   floor = new Sprite(250, 594, 190190, 20, 's');
+   floor.shapeColor = color(40, 140, 255, 0); 
+   floor.stroke = color(40, 140, 255, 0);
+
+   obstacle = new Sprite(100, 532, 100, 100, 's');
+
+
+   kitchen.overlaps(player);
+   corridor.overlaps(player);
+   corridor.layer = 1;
+   kitchen.layer = 1;
+   player.layer = 2;
+   floor.layer = 2;
+
 }
 
 function draw() {
    clear();
-   if (kb.presses("right")) {
-      player.vel.x = 3;
-   }
-   if (kb.presses("left")) {
-      player.vel.x = -3;
-   }
+
+   if (kb.pressing('left')) player.vel.x = -50;
+   else if (kb.pressing('right')) player.vel.x = 50;
+   else player.vel.x = 0;
+
    if (kb.presses("up")) {
       player.vel.y = -4;
-   }
-   if (kb.presses("down")) {
-      camera.zoom = 1.5;
    }
    if (player.collides(obstacle)) {
       player.vel.x = -5;
