ebce8484a8d77b49a5b5de99ddacd8db0b4a801a
Tof-jack25 <tournouxchristophe@hotmail.fr>
2023-07-06 13:24:07 +0200

    creation de la page accueil et game-over

--- sketch.js
+++ sketch.js
@@ -1,25 +1,49 @@
 let player, floor;
 
+
 function setup() {
    new Canvas(800, 600);
-   world.gravity.x = 10;
+
+   world.gravity.x = 0;
    world.gravity.y = 10;
 
-   player = new Sprite((0, 255, 0), 0);
+   corridor = new Sprite (250, 300, 6995, 600, 's');
+   corridor.img = "./assets/img/couloir.png";
+   corridor.scale = 0.56;
+
+  player = new Sprite(0, 0, 10, 10, 'd');
    player.img = "./assets/img/pngwing.com.png";
-   player.scale = 0.01;
-   floor = new Sprite(250, 200, 500, 20, "static");
-   obstacle = new Sprite(550, 180, 100, 100, "static");
+   player.scale = 0.05;
+   player.rotation = 0;
+
+
+   floor = new Sprite(250, 482, 6995, 1, 's');
+   floor.shapeColor = color(40, 140, 255, 0); 
+
+   
+   obstacle = new Sprite(100, 532, 100, 100, 's');
+
+   corridor.overlaps(player);
+   player.layer = 1;
+   floor.layer = 1;
+
 }
 
 function draw() {
    clear();
-   if (kb.presses("right")) {
-      player.vel.x = 3;
-   }
-   if (kb.presses("left")) {
-      player.vel.x = -3;
-   }
+
+   // if (kb.presses("right")) {
+   //    player.vel.x = 3;
+   // }
+   // if (kb.presses("left")) {
+   //    player.vel.x = -3;
+   // }
+   
+
+   if (kb.pressing('left')) player.vel.x = -10;
+   else if (kb.pressing('right')) player.vel.x = 10;
+   else player.vel.x = 0;
+
    if (kb.presses("up")) {
       player.vel.y = -4;
    }
